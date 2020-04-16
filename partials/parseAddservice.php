549997f80b1768614643a279b84f62e5b55fd0a5
Maxwell Morrison <maxkobbs@gmail.com>
2020-04-16 15:04:32 +0000

    Update: List, update, activate, deactivate

--- partials/parseAddservice.php
+++ partials/parseAddservice.php
@@ -30,19 +30,23 @@ if(isset($_POST['addserviceBtn'], $_POST['token'])){
     $user_id = $_POST['user_id'];
     $service_name = $_POST['service_name'];
     $service_des = $_POST['service_des'];
-    $service_price = $_POST['service_price'];
+		$service_price = $_POST['service_price'];
+		
+		// Check if activate service input field value is sent and also if the value is 1 then 
+		// set the $service_active variable to 1 and else set it to 0
+    $service_active = (isset($_POST['service_active']) && $_POST['service_active'] == 1 ) ? 1 : 0 ;
 
     //empty form errors, process form data and insert record
     if(empty($form_errors)){
       //SQL insert $statement
-      $sqlInsert = "INSERT INTO services (user_id, service_name, service_des, service_price)
-       VALUES (:user_id, :service_name, :service_des, :service_price)";
+      $sqlInsert = "INSERT INTO services (id, user_id, service_name, service_des, service_price, service_active)
+       VALUES (null, :user_id, :service_name, :service_des, :service_price, :service_active)";
 
        //use PDO prepare to sanitize data
        $statement = $db->prepare($sqlInsert);
 
        //add data into Database
-       $statement->execute(array(':user_id' => $user_id, ':service_name' => $service_name, ':service_des' => $service_des, ':service_price' => $service_price));
+       $statement->execute(array(':user_id' => $user_id, ':service_name' => $service_name, ':service_des' => $service_des, ':service_price' => $service_price, ':service_active' => $service_active));
 
          //check if one new row was created
          if($statement->rowCount() == 1){
@@ -71,6 +75,154 @@ if(isset($_POST['addserviceBtn'], $_POST['token'])){
 
   }
 
+	if(isset($_SESSION['id'])) {
+			
+		$id = $_SESSION['id'];
+
+		$sqlQuery = "SELECT * FROM services WHERE user_id = :id";
+		$statement = $db->prepare($sqlQuery);
+		$statement->execute(array(':id' => $id));
+
+		$services = $statement->fetchAll(PDO::FETCH_OBJ);
+
+	}
+
+	if(isset($_GET['action'], $_GET['id'])) {
+		
+		$id = $_SESSION['id'];
+		$sid = $_GET['id'];
+		$action = $_GET['action'];
+		$user_id = $_SESSION['id'];
+		
+		if( $action == 'edit' ) {
+
+			$sqlQuery = "SELECT * FROM services WHERE user_id = :id AND id = :sid ";
+			$statement = $db->prepare($sqlQuery);
+			$statement->execute(array(':id' => $id, ':sid' => $sid));
+
+			$serviceToUpdate = $statement->fetch(PDO::FETCH_OBJ);
+
+		}
+		if( $action == 'activate' ) {
+			//SQL insert $statement
+			$sqlUpdate = "UPDATE services SET service_active =:service_active WHERE user_id =:uid AND id =:sid ";
+
+			//use PDO prepare to sanitize data
+			$statement = $db->prepare($sqlUpdate);
+
+			//add data into Database
+			$statement->execute(array(':service_active' => 1, ':uid' => $user_id, ':sid' => $sid ));
+
+			//check if one new row was created
+			if($statement->rowCount() == 1){
+
+				$result = "<script type=\"text/javascript\">
+						swal({
+						title: \"UPDATED!\",
+						text: \"Service Updated Successfully\",
+						type: 'success',
+						confirmButtonText: \"OKay!\" });
+
+						setTimeout(() => {
+							window.location.assign('profile_service.php');
+						}, 1500)						
+				</script>";
+
+			}
+		}
+		if( $action == 'deactivate' ) {
+			//SQL insert $statement
+			$sqlUpdate = "UPDATE services SET service_active =:service_active WHERE user_id =:uid AND id =:sid ";
+
+			//use PDO prepare to sanitize data
+			$statement = $db->prepare($sqlUpdate);
+
+			//add data into Database
+			$statement->execute(array(':service_active' => 0, ':uid' => $user_id, ':sid' => $sid ));
+
+			//check if one new row was created
+			if($statement->rowCount() == 1){
+
+				$result = "<script type=\"text/javascript\">
+						swal({
+						title: \"UPDATED!\",
+						text: \"Service Updated Successfully\",
+						type: 'success',
+						confirmButtonText: \"OKay!\" });
+
+						setTimeout(() => {
+							window.location.assign('profile_service.php');
+						}, 1500)
+
+				</script>";
+
+			}
+		}
+	}
+
+	if(isset($_POST['updateserviceBtn'], $_POST['token'], $_GET['id'], $_GET['action'])){
+
+		$action = $_GET['action'];
+		$user_id = $_SESSION['id'];
+		$sid = $_GET['id'];
+		$service_name = $_POST['service_name'];
+		$service_des = $_POST['service_des'];
+		$service_price = $_POST['service_price'];
+
+		if( $action == 'edit' ) {
+			if(validate_token($_POST['token'])){
+
+				//store form error ArrayAccess
+				$form_errors = array();
+
+				//form validation
+				$required_fields = array('service_name', 'service_des', 'service_price');
+
+				//check empty Fields
+				$form_errors = array_merge($form_errors, check_empty_fields($required_fields));
+				
+				// Check if activate service input field value is sent and also if the value is 1 then 
+				// set the $service_active variable to 1 and else set it to 0
+				$service_active = (isset($_POST['service_active']) && $_POST['service_active'] == 1 ) ? 1 : 0 ;
+
+				//empty form errors, process form data and insert record
+				if(empty($form_errors)){
+					//SQL insert $statement
+					$sqlUpdate = "UPDATE services SET service_name =:service_name, service_des =:service_des, service_price =:service_price, service_active =:service_active WHERE user_id =:uid AND id =:sid ";
+
+					//use PDO prepare to sanitize data
+					$statement = $db->prepare($sqlUpdate);
+
+					//add data into Database
+					$statement->execute(array(':service_name' => $service_name, ':service_des' => $service_des, ':service_price' => $service_price, ':service_active' => $service_active, ':uid' => $user_id, ':sid' => $sid ));
+
+						//check if one new row was created
+						if($statement->rowCount() == 1){
+
+							$result = "<script type=\"text/javascript\">
+									swal({
+									title: \"UPDATED!\",
+									text: \"Service Updated Successfully\",
+									type: 'success',
+									confirmButtonText: \"OKay!\" });
+
+									setTimeout(() => {
+										window.location.assign('profile_service.php');
+									}, 1500)
+							</script>";
+
+						}else{
+							if(count($form_errors) == 1){
+									$result = flashMessage("There was 1 error in the form<br>");
+							}else{
+									$result = flashMessage("There were " .count($form_errors). " errors in the form <br>");
+							}
+						}
+
+				}
+			}
+		}
+	}
 
 
  ?>
--- profile_service.php
+++ profile_service.php
@@ -41,7 +41,7 @@ include_once 'partials/parseAddservice.php';
           </li>
         </ul><br>
         <div class="card p-2">
-    <h4>Add Your Services</h4>
+    <h4><?php echo ( isset($serviceToUpdate) && !empty($serviceToUpdate->id) ) ? 'Update Service ('.$serviceToUpdate->service_name.')' : 'Add New Service' ?></h4>
     <div>
         <?php if(isset($result)) echo $result; ?>
         <?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
@@ -49,19 +49,23 @@ include_once 'partials/parseAddservice.php';
         <form action="" method="post">
           <div class="form-group">
         <label for="">Salon Service Name</label>
-        <input type="text" name="service_name" class="form-control" autocomplete=" required">
+        <input type="text" name="service_name" class="form-control" autocomplete=" required" value="<?php echo ( isset($serviceToUpdate) && !empty($serviceToUpdate->service_name) ) ? $serviceToUpdate->service_name : '' ?>">
       </div>
       <div class="form-group">
         <label for="">Service Description</label>
-        <input type="text" name="service_des" class="form-control">
+        <input type="text" name="service_des" class="form-control" value="<?php echo ( isset($serviceToUpdate) && !empty($serviceToUpdate->service_des) ) ? $serviceToUpdate->service_des : '' ?>">
       </div>
       <div class="form-group">
         <label for="">Price</label>
-        <input type="text" name="service_price" class="form-control">
+        <input type="text" name="service_price" class="form-control" value="<?php echo ( isset($serviceToUpdate) && !empty($serviceToUpdate->service_price) ) ? $serviceToUpdate->service_price : '' ?>">
+      </div>
+      <div class="form-group">
+        <label for="activate_service">Activate</label>
+        <input type="checkbox" name="service_active" id="activate_service" class="form-control" value="1" <?php echo ( isset($serviceToUpdate) && !empty($serviceToUpdate->service_active) ) ? 'checked' : '' ?>>
       </div>
       <input type="hidden" name="user_id" value="<?php if(isset($id)) echo $id; ?>">
       <input type="hidden" name="token" value="<?php if(function_exists('_token')) echo _token(); ?>">
-      <button type="submit" name="addserviceBtn" class="btn btn-primary">Submit</button>
+      <button type="submit" name="<?php echo ( isset($serviceToUpdate) && !empty($serviceToUpdate->id) ) ? 'updateserviceBtn' : 'addserviceBtn' ?>" class="btn btn-primary"><?php echo ( isset($serviceToUpdate) && !empty($serviceToUpdate->id) ) ? 'Update' : 'Submit' ?></button>
         </form>
 
       </div><br><br>
@@ -80,24 +84,32 @@ include_once 'partials/parseAddservice.php';
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Larry</td>
-                  <td>the Bird</td>
-                  <td>@twitter</td>
-                </tr>
+							<?php 
+								if( count($services) > 0): 
+									foreach( $services as $service ):
+							?>
+									<tr>
+										<th><?php echo $service->service_name; ?></th>
+										<td><?php echo $service->service_des; ?></td>
+										<td><?php echo $service->service_price; ?></td>
+										<td>
+											<a href="profile_service.php?action=edit&id=<?php echo $service->id ?>" class="btn btn-primary mb-2">Edit</a>
+											<a href="profile_service.php?action=delete&id=<?php echo $service->id ?>" class="btn btn-danger mb-2">Delete</a>
+											<?php if( $service->service_active == 1 ): ?>
+												<a href="profile_service.php?action=deactivate&id=<?php echo $service->id ?>" class="btn btn-warning mb-2">Deactivate</a>
+											<?php elseif( $service->service_active == 0 ): ?>
+												<a href="profile_service.php?action=activate&id=<?php echo $service->id ?>" class="btn btn-success mb-2">Activate</a>
+											<?php endif; ?>
+										</td>
+									</tr>
+							<?php 
+									endforeach;
+								else:
+							?>
+								<tr>
+										<th colspan="4">No services found.</th>
+								</tr>
+							<?php endif; ?>
               </tbody>
             </table>
           </div>
