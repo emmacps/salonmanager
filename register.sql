284bde5e71f3e94f875c0baf3b7dd0a2b816f80b
emmacps <emmacps001@gmail.com>
2020-04-17 03:01:19 +0000

    db with salon services

--- register.sql
+++ register.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 15, 2020 at 02:40 AM
+-- Generation Time: Apr 17, 2020 at 02:59 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -40,6 +40,35 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `services`
+--
+
+DROP TABLE IF EXISTS `services`;
+CREATE TABLE IF NOT EXISTS `services` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `service_name` varchar(255) NOT NULL,
+  `service_des` varchar(255) NOT NULL,
+  `service_price` varchar(255) NOT NULL,
+  `service_active` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `services`
+--
+
+INSERT INTO `services` (`id`, `user_id`, `service_name`, `service_des`, `service_price`, `service_active`) VALUES
+(1, 0, 'fdgf', 'ggfgf', 'gsgs', ''),
+(2, 0, 'tytyt', 'tryhy', 'tdhd', ''),
+(3, 0, 'utyt', 'ityuityu', 'ityyuiti', ''),
+(4, 9, 'yuy11', 'yuyu1', '265', '1'),
+(5, 9, 'wig', 'natual wig', 'Ghc200', '1'),
+(6, 12, 'yy', 'jgd', 'fg', '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `trash`
 --
 
@@ -71,17 +100,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `shop_name`, `des`, `username`, `password`, `email`, `activated`, `avatar`, `join_date`) VALUES
-(7, '', '', 'emmacps', '$2y$10$VcYrUSUqgMJPC9PKjN2hW.XiMKYaq.mPxw1g.u4N2BdNdDnk4ePoq', 'emmacps001@gmail.com', '1', 'uploads/emmacpse9fe98669149a422981e5c1a96d7fd09.jpg', '2020-02-19 23:10:21'),
-(8, '', '', 'wandy', '$2y$10$EueMpHmUbPubWRFD7wCFj.Z1QEdjv6svAPwJEP8XkTAV1pVvUu8lm', 'wandy@gmail.com', '1', 'uploads/wandy2f95e6a98d5ec6657dbbd30f6ae0331b.png', '2020-03-05 03:52:54'),
-(9, 'tech home salon', '                          Testing this box   for salon description                  ', 'samuel001', '$2y$10$1Gn2gK3xYY5qT1jKZGUmJ.OV7I.BP3pRjU8NjhFcW9CwsdxFXOmD.', 'samuelasare0001@gmail.com', '1', 'uploads/samuelb8b61ef66cff7ab1555d28a29dcc86a6.jpg', '2020-04-14 01:32:42'),
-(10, '', '', 'festus', '$2y$10$cDA/qlNb9ybGoQVyRM22L.4oHUPr5gOnMOpVS/XiH/cuGqfmRe.iW', 'festus001@gmail.com', '0', 'uploads/default.jpg', '2020-03-14 03:21:57');
+(9, 'tech home salon 22', '                                                                              Testing this box   for salon description                                                                  ', 'samuel001', '$2y$10$1Gn2gK3xYY5qT1jKZGUmJ.OV7I.BP3pRjU8NjhFcW9CwsdxFXOmD.', 'samuelasare0001@gmail.com', '1', 'uploads/samuelb8b61ef66cff7ab1555d28a29dcc86a6.jpg', '2020-04-15 04:06:32'),
+(12, 'Waves Salons', 'A beautiful salon for u                                           ', 'zandy001', '$2y$10$GNpEKhXjwvPl0XV0PHNCne6sDHeJkpBavWOeaMn2Ahcaop2f2QJo6', 'samuelas001@yahoo.com', '1', 'uploads/zandy0015e639f5d1fd69e44030a1c9371842eb1.jpg', '2020-04-16 02:09:07'),
+(11, 'Your Shop Name', 'Short Description', 'kanny001', '$2y$10$6.zXyPpD9bXBCiMfCDII8OB1EfUBmqs2CHVAGJq7kgSQ.1y.4AXjC', 'emmasco20032003@gmail.com', '1', 'uploads/default.jpg', '2020-04-17 00:36:50');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
